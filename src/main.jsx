da91f6b31d35624fb05a3e4c0127e685b721f044
unknown <niloyp10@gmail.com>
2024-05-11 21:53:54 +0600

    homepageset

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Root from "./components/Root/Root";
+import Homepage from "./components/Homepage/Homepage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children : [
+      {
+        path : "/",
+        element : <Homepage></Homepage>,
+      }
+    ]
   },
 ]);
 
