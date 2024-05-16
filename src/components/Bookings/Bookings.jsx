121c2fc954b2e9ebd10d27fa8e0162cf28493f0b
unknown <niloyp10@gmail.com>
2024-05-17 00:45:55 +0600

    value added done

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Bookings = () => {
+
+    const [cities] = useState(['Dhaka', "Chittagong", "Sylhet"])
+    const [fromcity, setfromcity] = useState('')
+    const [tocity, settocity] = useState('');
+    
+    
+    const handlesubmit = event => {
+        event.preventDefault()
+        const from = fromcity;
+        const to = tocity
+        console.log(from, to)
+    }
+
     return (
         <div>
-            this is the bookings page
+            <form onSubmit={handlesubmit}>
+                <select onChange={e=>setfromcity(e.target.value)} name="" id="">
+                    {
+                        cities.map(city=>{
+                            return <option key={city} value={city}>{city}</option>
+                        })
+                    }
+                </select>
+                <select onChange={(e)=>settocity(e.target.value)} name="" id="">
+                    {
+                        cities.filter((city)=>{
+                            return city != fromcity
+                        }).map(item=>{
+                            return <option key={item}  value={item}>{item}</option>
+                        })
+                    }
+                </select>
+                <button type="submit">submit</button>
+            </form>
         </div>
     );
 };
